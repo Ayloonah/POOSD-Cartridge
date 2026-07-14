90555e0f58b4271638470d5c3fac8aba7bbe9f5f
KingDame04 <damienway2023@gmail.com>
2026-07-14 02:00:08 -0400

    Add files via upload

---
+++ backend/src/routes/listRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.router();
+const{
+    createList,
+    getUserLists,
+    deleteList
+} = require("../controllers/listController");
+
+router.post("/", createList);
+router.get("/user/:userId", getUserLists);
+router.delete("/user/:userId/:listId", deleteList);
+module.exports = router;
