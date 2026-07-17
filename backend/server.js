cb284e35fcb22298c361696ee5eda82b22709524
KingDame04 <damienway2023@gmail.com>
2026-07-17 02:27:51 -0400

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./src/routes/auth");
+const listRoutes = require("./src/routes/listRoutes");
+const gameRoutes = require("./src/routes/gameRoutes");
+
+const app = express();
+app.use(cors());
+// Allows your backend to read JSON request bodies
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+// API routes
+const apiRouter = express.Router();
+apiRouter.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+
+// Mount auth routes to API router
+apiRouter.use("/auth", authRoutes);
+apiRouter.use("/lists", listRoutes)
+apiRouter.use("/games", gameRoutes);
+app.use("/api", apiRouter);
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
