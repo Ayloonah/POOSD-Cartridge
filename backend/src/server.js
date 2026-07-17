772f5211c3e42e22a4b7a14214550af5147da8ae
KingDame04 <damienway2023@gmail.com>
2026-07-17 02:03:31 -0400

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,4 @@
 const path = require("path");
-
-require("dotenv").config({
-    path: path.resolve(__dirname, "../.env")
-});
-
 const express = require("express");
 const mongoose = require("mongoose");
 
